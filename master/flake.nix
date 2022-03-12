ccc6da1943abee1b0ed972c28d3e416a28d341b7
hugosenari <hugosenari@gmail.com>
2022-03-12 04:31:03 -0300

    chore: re index nimrax

---
+++ master/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = ''Radix tree wrapper for Nim'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-nimrax-master.flake = false;
+  inputs.src-nimrax-master.owner = "genotrance";
+  inputs.src-nimrax-master.ref   = "refs/heads/master";
+  inputs.src-nimrax-master.repo  = "nimrax";
+  inputs.src-nimrax-master.type  = "github";
+  
+  inputs."nimgen".dir   = "nimpkgs/n/nimgen";
+  inputs."nimgen".owner = "riinr";
+  inputs."nimgen".ref   = "flake-pinning";
+  inputs."nimgen".repo  = "flake-nimble";
+  inputs."nimgen".type  = "github";
+  inputs."nimgen".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nimgen".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-nimrax-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-nimrax-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
